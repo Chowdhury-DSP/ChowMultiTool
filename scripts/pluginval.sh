d178ba0df316f69a3f0f3708c0d6c16460d184fc
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2023-01-15 15:30:35 -0800

    More work on validation scripts

--- scripts/pluginval.sh
+++ scripts/pluginval.sh
@@ -33,7 +33,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # declare -a plugins=()
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     pluginval=$(install_pluginval_mac)
-    declare -a plugins=("build/ChowMultiTool_artefacts/Debug/VST3/ChowMultiTool.vst3")
+    declare -a plugins=("build/ChowMultiTool_artefacts/Release/VST3/ChowMultiTool.vst3")
 else
     pluginval=$(install_pluginval_win)
     declare -a plugins=("build/ChowMultiTool_artefacts/Release/VST3/ChowMultiTool.vst3")
