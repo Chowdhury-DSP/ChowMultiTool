733506a32375faa69720654b2ad27b8ab38f95a5
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2023-03-16 17:57:14 -0700

    Improvements for waveshaper UI (#6)
    * Fixing ranges for waveshaper plot and adding grid lines
    
    * Add control menus
    
    * Apply clang-format
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/gui/Waveshaper/WaveshaperEditor.cpp
+++ src/gui/Waveshaper/WaveshaperEditor.cpp
@@ -1,19 +1,33 @@
 #include "WaveshaperEditor.h"
+#include "WaveshaperColours.h"
 
 namespace gui::waveshaper
 {
 WaveshaperEditor::WaveshaperEditor (State& pluginState, dsp::waveshaper::Params& wsParams)
     : plot (pluginState, wsParams),
+      bottomBar (pluginState, wsParams),
       paramsView (pluginState, wsParams)
 {
     addAndMakeVisible (plot);
+    addAndMakeVisible (bottomBar);
     addAndMakeVisible (paramsView);
 }
 
 void WaveshaperEditor::resized()
 {
     auto bounds = getLocalBounds();
-    plot.setBounds (bounds.removeFromTop (proportionOfHeight (0.5f)));
-    paramsView.setBounds (bounds);
+    paramsView.setBounds (bounds.removeFromBottom (proportionOfHeight (0.15f)));
+    bottomBar.setBounds (bounds.removeFromBottom (proportionOfHeight (0.075f)));
+    plot.setBounds (bounds);
+}
+
+void WaveshaperEditor::paint (juce::Graphics& g)
+{
+    g.setGradientFill (juce::ColourGradient { colours::backgroundLight,
+                                              juce::Point { 0.0f, 0.0f },
+                                              colours::backgroundDark,
+                                              juce::Point { (float) getWidth() * 0.35f, (float) getHeight() * 0.5f },
+                                              false });
+    g.fillAll();
 }
 } // namespace gui::waveshaper
--- src/gui/Waveshaper/WaveshaperEditor.h
+++ src/gui/Waveshaper/WaveshaperEditor.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "BottomBar.h"
 #include "WaveshaperPlot.h"
 
 namespace gui::waveshaper
@@ -9,10 +10,12 @@ class WaveshaperEditor : public juce::Component
 public:
     WaveshaperEditor (State& pluginState, dsp::waveshaper::Params& wsParams);
 
+    void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
     WaveshaperPlot plot;
+    BottomBar bottomBar;
     chowdsp::ParametersView paramsView;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WaveshaperEditor)
